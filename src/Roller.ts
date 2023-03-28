71463b88823ae8065b5a4badc30df6cf8497a2ba
shalavritha mamunooru <shalavritha.srtforever@gmail.com>
2023-03-28 10:51:28 -0700

    Quiz Completed

--- src/Roller.ts
+++ src/Roller.ts
@@ -1,35 +1,59 @@
 export class Roller {
-    private distribution: Record<number, number>;
-    private lastRoll: number;
-    readonly faces: number;
-  
-    constructor(faces: number = 6) {
-      if (!Number.isInteger(faces) || faces < 2) {
-        throw new Error('Invalid number of faces.');
-      }
-      this.faces = faces;
-      this.lastRoll = 0;
-      this.distribution = Array.from({ length: faces }, (_, i) => [i + 1, 0])
-        .reduce((acc, [face, count]) => {
-          acc[face] = count;
-          return acc;
-        }, {} as Record<number, number>);
+    private _distribution: Map<number, number>;
+    private _last: number;
+    private _faces: number;
+
+    // specifies the number of faces the die has. It must have at least 2 faces
+    // so any invalid values should default to 6 to represent the common 
+    // six-sided die.
+    constructor(faces: number) {
+        if (faces >= 2) {
+            this._faces = faces;
+        } else {
+            this._faces = 6;
+        }
+        this._distribution = new Map<number, number>();
+        for (let i = 1; i <= this._faces; i++) {
+            this._distribution.set(i, 0);
+        }
     }
-  
+
+    // specifies which number should be rolled and returns the recorded roll. 
+    // If the value provided is not valid for the number of faces the die has,
+    // no record of the roll should be made and 0 should be returned. However,
+    // if the value provided is between 1 and the number of faces (inclusive),
+    // then:
+    //   1. that same value should be returned,
+    //   2. that value should be stored as the last roll, and
+    //   3. the distribution should update the distribution record so that the
+    //      key (value rolled) has an updated value that increases the number
+    //      of rolls for that face by 1
+    private lastRoll: number = 0;
+
     roll(value: number): number {
-      if (!Number.isInteger(value) || value < 1 || value > this.faces) {
-        return 0;
-      }
-      this.lastRoll = value;
-      this.distribution[value]++;
-      return value;
+        if (value < 1 || value > this._faces) {
+            return 0;
+        }
+        this.lastRoll = value;
+        const count = this._distribution.get(value);
+        if (count !== undefined) {
+            this._distribution.set(value, count + 1);
+        }
+        return value;
     }
-  
-    getLastRoll(): number {
-      return this.lastRoll;
+
+    // returns the value of the latest die roll. If no rolls have been made
+    // yet, then it should return 0.
+    last(): number {
+        return this.lastRoll;
     }
-  
-    getDistribution(): Record<number, number> {
-      return { ...this.distribution };
+
+    // returns a Map that represents the current distribution of rolls for each
+    // possible face that can be rolled. The map's key should be the face
+    // value and its value should be the number of times that face has been
+    // rolled. Even if a face has not yet been rolled, its key should have a
+    // value of 0.
+    distribution(): Map<number, number> {
+        return new Map<number, number>(this._distribution);
     }
-  }
+}
